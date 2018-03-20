a645018842f245faf4d34f102449c0b32c94c27c
samuelGabbai <samgab770@gmail.com>
2018-03-20 20:27:46 +0100

    Update HttpRequest without URL Connection

--- src/process/ProcessUnit.java
+++ src/process/ProcessUnit.java
@@ -1,68 +1,94 @@
 package process;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.Scanner;
+import java.net.InetSocketAddress;
+import java.nio.channels.SocketChannel;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.concurrent.Callable;
 import java.util.logging.Level;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
+import HttpRequest.HttpGet;
+import io.SerializerBuffer;
 import util.Cheat;
 
 public class ProcessUnit implements Callable<Set<String>>{
-	private final static String HREF_REGEX = "href=\"(/[^\"]*)";
-	private final static Pattern pattern = Pattern.compile(HREF_REGEX);
-	private final static String AMP_ESCAPED = "&amp;";
-	private final static String AMP_ESCAPED_REPLACEMENT = "";
 	
-	private final String hostname;
+	
 	private final Set<String> links;
+	private final SerializerBuffer sb = new SerializerBuffer() ;
 	
-	public ProcessUnit(String hostname) {
+	private String hostname;
+	private String relativLink;
+	private SocketChannel s;
+	
+	
+	public ProcessUnit(String hostname , String relativLink) {
 		this.hostname = hostname;
+		this.relativLink = relativLink;
 		this.links = new TreeSet<>();
-	}
-	
-	private URLConnection connect(String hostname) throws IOException {
-		URL url = new URL(hostname);
-		URLConnection uc = url.openConnection();
-		return uc;
-	}
-	
-	private String format(String s) {
-		return s.replaceAll(AMP_ESCAPED, AMP_ESCAPED_REPLACEMENT);
-	}
-	
-	private Set<String> getContent(InputStream in) {
-		Scanner scanner = new Scanner(in);
-		String line, urlMatched;
-		Matcher m;
-		while(scanner.hasNextLine()) {
-			line = scanner.nextLine();
-			m = pattern.matcher(line);
-			while(m.find()) {
-				urlMatched = format(m.group(1));
-				Cheat.LOGGER.log(Level.FINEST, "URL found: " + urlMatched);
-				links.add(urlMatched);
-			}
-		}
-		scanner.close();
-		return links;
 	}
 	
 	@Override
 	public Set<String> call() throws IOException {
-		Cheat.LOGGER.log(Level.FINE, "Unit " + Thread.currentThread().getId() + " is starting");
+		//connect(hostname);
+		createSocket();
+		generateRequest();
+		//HttpRequest OK/RDir = generateRequest(link,hostname,SerializableBuffer): OK -> PARSE | RDIR -> CONNECT
+		
+		//Set<String> = parse(SerializableBuffer)
+		
+		return links;
+		/*Cheat.LOGGER.log(Level.FINE, "Unit " + Thread.currentThread().getId() + " is starting");
 		URLConnection uc = connect(hostname);
 		Cheat.LOGGER.log(Level.FINE, "Unit " + Thread.currentThread().getId() + " connected to " + hostname);
 		Set<String> links = getContent(uc.getInputStream());
-		Cheat.LOGGER.log(Level.INFO, "Unit " + Thread.currentThread().getId() + " has found " + links.size() + " links from url: " + hostname);
-		return links;
+		Cheat.LOGGER.log(Level.FINE, "Unit " + Thread.currentThread().getId() + " has found " + links.size() + " links from url: " + hostname);
+		return links;*/
+	}
+	
+	
+	private void createSocket() throws IOException {
+		s = SocketChannel.open(new InetSocketAddress(hostname, 80));
+	}
+
+	
+	private void generateRequest() throws IOException {
+		Cheat.LOGGER.log(Level.INFO, "Create HTTP GET");
+		createHttpGet();
+		Cheat.LOGGER.log(Level.INFO, "Write on Serializable Buffer");
+		socketWrite();
+	}
+	
+	
+	
+	private void createHttpGet() {
+		HttpGet hget = new HttpGet(hostname,relativLink);
+		hget.writeToBuff(sb);
+	}
+	
+	private void socketWrite() throws IOException {
+		s.write(sb.getBuffer());
+	}
+	
+	private void socketRead() throws IOException {
+		sb.clear();
+		sb.read(s);
+		sb.flip();
+		System.out.println(Cheat.CHARSET.decode(sb.getBuffer()));
+	}
+	
+
+
+	public static void main(String[] args) throws IOException {
+		ProcessUnit pu = new ProcessUnit("www.onisep.fr","/");
+		Cheat.LOGGER.log(Level.INFO, "Send create Socket");
+		pu.createSocket();
+		Cheat.LOGGER.log(Level.INFO, "Generate Socket");
+		pu.generateRequest();
+		Cheat.LOGGER.log(Level.INFO, "Read on Socket");
+		pu.socketRead();
+
 	}
 	
 }
